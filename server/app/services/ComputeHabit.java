2ad1590e3f68169f5fcf2b3f7568ffed08160a2f
minh <minh.schils@student.uliege.be>
2019-03-01 13:50:43 +0100

    clustering algo

--- server/app/services/ComputeHabit.java
+++ server/app/services/ComputeHabit.java
@@ -1,71 +1,157 @@
 package services;
+
+import ca.pfv.spmf.algorithms.timeseries.autocorrelation.AlgoLagAutoCorrelation;
+import ca.pfv.spmf.algorithms.timeseries.TimeSeries;
 import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Arrays;
-import ca.pfv.spmf.algorithms.timeseries.autocorrelation.AlgoLagAutoCorrelation;
-import ca.pfv.spmf.algorithms.timeseries.TimeSeries;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
 
-public class ComputeHabit{
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.ml.clustering.DBSCANClusterer;
+import org.apache.commons.math3.ml.clustering.DoublePoint;
+import org.apache.commons.math3.ml.clustering.Cluster;
+
+public class ComputeHabit {
 
     ArrayList<Long> raw_data;
     Long[] index;
     Long base;
     double[] signal;
     long scale;
+    int minimal_period = 1440;
 
-    ComputeHabit(ArrayList<Long> array,long scale){ 
+    public ComputeHabit(ArrayList<Long> array, long scale) {
         Collections.sort(array);
+        System.out.println(array);
         this.scale = scale;
         raw_data = array;
         base = array.get(0);
-        int signal_size = Math.toIntExact((array.get(array.size()-1) - base) / scale) + 1;
+        int signal_size = Math.toIntExact((array.get(array.size() - 1) - base) / scale) + 1;
+        System.out.println("signal_size" + signal_size);
         index = new Long[array.size()];
-        Iterator<Long> ite = array.iterator(); 
+        Iterator<Long> ite = array.iterator();
         signal = new double[signal_size];
         int i = 0;
         int j = 0;
-
-        //format data to a signal for the autocorrelation.       
-        while(ite.hasNext()){
+        // format data to a signal for the autocorrelation.
+        while (ite.hasNext()) {
             index[i] = (ite.next() - base) / scale;
-            System.out.println("i= " + index[i]);
-            System.out.println("j= " + j);
-            while(j < index[i]){
+            while (j < index[i]) {
                 signal[j] = 0;
                 j++;
             }
-            //skip repeated value in input signal => should never happend.
-            if(i > 0 && index[i] == index[i-1] ){
-                i++;
-                continue;
+            // skip repeated value in input signal => should never happend.
+            if (j < signal.length && (i == 0 || index[i] != index[i - 1])) {
+                signal[j] = 1;
+                j++;
             }
-            signal[j] = 1;
-            j++;
             i++;
         }
-        System.out.println(Arrays.toString(signal));
     }
-    
-    public double[] getSignal(){
+
+    public double[] getSignal() {
         return this.signal;
     }
 
-    void getHabit() throws IOException{
-        AlgoLagAutoCorrelation autocorr = new AlgoLagAutoCorrelation();
-        TimeSeries result = autocorr.runAlgorithm(new TimeSeries(signal,"raw data"),signal.length/2);
-        System.out.println(result.toString());
-        Double[] results = new Double[result.data.length];
-        for(int i=0; i<result.data.length;i++){
-            results [i] = Double.valueOf(result.data[i]);
-        }        
-        IndexSorter<Double> is = new IndexSorter<Double>(results);
-        is.sort();
-        Integer[] ranked_period = is.getIndexes();
-        System.out.println(Arrays.toString(ranked_period));
-
+    public void getHabit() throws IOException {
+        LinkedHashSet<Integer> periods = find_period();
+        System.out.println(Arrays.toString(periods.toArray()));
+        Iterator<Integer> ite = periods.iterator();
+        ArrayList<ArrayList<DoublePoint>> indexes = new ArrayList<ArrayList<DoublePoint>>();
+        ArrayList<int[]> counts = new ArrayList<int[]>();
+        while (ite.hasNext()) {
+            Integer p = ite.next();
+            int[] count = new int[signal.length / p];
+            indexes.add(format_signal(p, count));
+            counts.add(count);
+        }
+        System.out.println(indexes.get(0).toString());
     }
 
+    private double mean(int[] array) {
+        int total = 0;
+        for (int i = 0; i < array.length; i++) {
+            total += i;
+        }
+        return (double) total / (double) array.length;
+    }
+    private ArrayRealVector toRealVector(ArrayList<DoublePoint> d){
+        double[] array = new double[d.size()];
+        for(int i=0;i<d.size();i++){
+            array[i] = d.get(i).getPoint()[0];
+        }
+        return new ArrayRealVector(array);
+    }
+
+    private LinkedHashSet<Integer> find_period() throws IOException {
+        // period detection with autocorrelation
+        AlgoLagAutoCorrelation autocorr = new AlgoLagAutoCorrelation();
+        TimeSeries result = autocorr.runAlgorithm(new TimeSeries(signal, "raw data"), signal.length / 2);
+        result.data[0] = 0.0;
+        IndexSorter is = new IndexSorter(result.data);
+        is.sort(false);
+        Integer[] ranked_period = is.getIndexes();
+        LinkedHashSet<Integer> periods = new LinkedHashSet<Integer>();
+        for (int i = 0; i < (int) ranked_period.length * 0.05 + 1; i++) {
+            int candidate = Math.round(ranked_period[i] / minimal_period) * minimal_period;
+            if (!periods.contains(candidate)) {
+                periods.add(candidate);
+            }
+        }
+        return periods;
+    }
+
+    // prepare the data for the clustering algorithm.
+    // gives the index of one of the signal splitted at period.
+    // Ex: this.signal = 1 0 0 1 0 0 0 1 1 0 0 1 and period = 4
+    // splitted signal: 1 0 0 1 || 0 0 0 1 || 1 0 0 1 ||
+    // out => 0 0 3 3 3
+
+    // count is a zero vectorof size signal.length/period.
+    // count will containt the number of non zero element in the split.
+    // count => [2,1,2]
+    private ArrayList<DoublePoint> format_signal(int period, int[] count) {
+        ArrayList<DoublePoint> out = new ArrayList<DoublePoint>();
+        for (int i = 0; i < period - 1; i++) {
+            for (int j = 0; j < signal.length - period; j += period) {
+                if (signal[i + j] == 1.0) {
+                    System.out.println("coucou");
+                    int[] p = { i };
+                    out.add(new DoublePoint(p));
+                    count[j / period]++;
+                }
+            }
+        }
+        return out;
+    }
+
+    // return the cluster partitionning of index for a given period.
+    private List<Cluster<DoublePoint>> cluster(ArrayList<DoublePoint> index, int period, int[] count) {
+        LinkedList<Cluster<DoublePoint>> out = new LinkedList<Cluster<DoublePoint>>();
+        // impose a minimum reliability of 50%
+        Integer min_point = signal.length / (2 * period);
+
+        // try to get an idea of a good epsilon value for dbscan.
+        ArrayRealVector original = toRealVector(index);
+        ArrayRealVector shift = (ArrayRealVector) original.getSubVector(1, original.getDimension() - 2);
+        shift = (ArrayRealVector) shift.append(original.getEntry(0) + period);
+        ArrayRealVector diff = (ArrayRealVector) shift.subtract(original);
+        double nb_cluster = mean(count);
+        IndexSorter is = new IndexSorter(diff.toArray());
+        is.sort(true);
+        Integer[] sorted_id = is.getIndexes();
+        int eps_id = (int) ((min_point) * Math.round(nb_cluster));
+        double epsilon = diff.getEntry(sorted_id[eps_id]);
+
+        // format input
+        DBSCANClusterer<DoublePoint> dbscan = new DBSCANClusterer<DoublePoint>(epsilon, min_point);
+        return dbscan.cluster(index);
+    }
 
 }
--- server/app/services/IndexSorter.java
+++ server/app/services/IndexSorter.java
@@ -7,41 +7,24 @@ package services;
  *
  */
 
- //copy from: https://www.algosome.com/articles/sort-array-index-java.html
+ //copy from: https://www.algosome.com/articles/sort-array-index-java.html.
+ //Modified for dealing only with double[], and allows ascendant or descandant sorting.
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Collections;
 
-public class IndexSorter<T extends Comparable<T>> implements Comparator<Integer> {
+public class IndexSorter implements Comparator<Integer> {
 
-    private final T[] values;
+    private final double[] values;
     private final Integer[] indexes;
 
     /**
      * Constructs a new IndexSorter based upon the parameter array.
-     * 
      * @param d
      */
-    public IndexSorter(T[] d) {
+    public IndexSorter(double[] d) {
         this.values = d;
-        indexes = new Integer[this.values.length];
-        for (int i = 0; i < indexes.length; i++) {
-            indexes[i] = i;
-        }
-    }
-
-    /**
-     * Constructs a new IndexSorter based upon the parameter List.
-     * 
-     * @param d
-     */
-    public IndexSorter(List<T> d) {
-        this.values = (T[]) d.toArray();
-        for (int i = 0; i < values.length; i++) {
-            values[i] = d.get(i);
-        }
         indexes = new Integer[this.values.length];
         for (int i = 0; i < indexes.length; i++) {
             indexes[i] = i;
@@ -52,8 +35,13 @@ public class IndexSorter<T extends Comparable<T>> implements Comparator<Integer>
      * Sorts the underlying index array based upon the values provided in the
      * constructor. The underlying value array is not sorted.
      */
-    public void sort() {
-        Arrays.sort(indexes, Collections.reverseOrder(this));
+    public void sort(boolean ascendant) {
+        if(ascendant){
+            Arrays.sort(indexes,this);
+        }
+        else{
+            Arrays.sort(indexes, Collections.reverseOrder(this));
+        }
     }
 
     /**
@@ -66,33 +54,21 @@ public class IndexSorter<T extends Comparable<T>> implements Comparator<Integer>
         return indexes;
     }
 
-    public T[] get_value(){
+    public double[] get_value(){
         return this.values;
     }
 
     /**
-     * 
      * Compares the two values at index arg0 and arg0
-     * 
      * @param arg0 The first index
-     * 
      * @param arg1 The second index
-     * 
-     * @return The result of calling compareTo on T objects at position arg0 and
-     *         arg1
-     * 
+     * @return The result of calling compareTo on T objects at position arg0 and arg1
      */
 
     @Override
-
     public int compare(Integer arg0, Integer arg1) {
-
-        T d1 = values[arg0];
-
-        T d2 = values[arg1];
-
+        Double d1 = values[arg0];
+        Double d2 = values[arg1];
         return d1.compareTo(d2);
-
     }
-
 }
